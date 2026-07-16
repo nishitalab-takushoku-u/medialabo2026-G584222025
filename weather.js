91b5f2d46c52dd2c97be7d55b847d0626085bd20
Usa Yuri <258422@st.takushoku-u.ac.jp>
2026-07-16 17:13:42 +0900

    仮提出

--- weather.js
+++ weather.js
@@ -1,19 +1,12 @@
 
-// 課題3-2 のプログラムはこの関数の中に記述すること
-/*function print(data) {
-  console.log('緯度：'+data.coord.lon);
-  console.log('経度：'+data.coord.lat);
-  console.log('天気：'+data.weather[0].description);
-  console.log('最低気温：'+data.main.temp_min);
-  console.log('最高気温：'+data.main.temp_max);
-  console.log('湿度：'+data.main.humidity);
-  console.log('風速：'+data.wind.speed);
-  console.log('風向：'+data.wind.deg);
-  console.log('都市名：'+data.name);
-}*/
+let id; //入力された都市名
 
 // 課題5-1 の関数 printDom() はここに記述すること
 function printDom(data) {
+  let old = document.querySelector("#result"); 
+  if (old) { 
+    old.remove(); 
+  } 
   let div = document.createElement('div');
   let body = document.querySelector('body');
   body.insertAdjacentElement('beforeend', div);
@@ -74,28 +67,105 @@ function printDom(data) {
   }
 
   let img = document.createElement("img");
-  img.src = "https://tsukatte.com/wp-content/uploads/2021/06/cloudy.png";
+
+  if (id === 360630) {
+  } else if (id === 524901) {
+    img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf3_9Eada3d-XkfRd_v8KAnGll9IsV1x9uvqnz6hy7kQ&s=10';
+  } else if (id === 993800) {
+    img.src = 'https://dol.ismcdn.jp/mwimgs/d/e/-/img_60566193d774cf8fe9ca29e12f06591e286416.jpg';
+  } else if (id === 1816670) {
+    img.src = 'https://images.contentstack.io/v3/assets/blt06f605a34f1194ff/bltaf0ccbb8ac3870bc/6878ad7053a3363f4054ace8/iStock-813195980-header_mobile.jpg?fit=crop&auto=webp&quality=60&crop=smart&format=avif';
+  } else if (id === 1850147) {
+    img.src = 'https://cdn.kinnso.ai/laravel/posts/20241231-beijing/content/1735812533_Screenshot_2025-01-02_at_6.08.36%E2%80%AFPM.png';
+  } else if (id === 1880252) {
+    img.src = 'https://res.klook.com/image/upload/fl_lossy.progressive,q_60/Mobile/City/lgra7vcg7b2g3zcts3sl.jpg';
+  } else if (id === 2147714) {
+    img.src = 'https://www.veltra.com/jp/guide/wp-content/uploads/2023/10/pixta_40776811_M.jpg';
+  } else if (id === 2643743) {
+    img.src = 'https://img-www.knt.co.jp/travelguide/kaigai/image/travelguide_020_mv.png';
+  } else if (id === 2968815) {
+    img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4qsJ1UenfU-JqVudcbrKPYeTFQXrVyCheAQlbz3uKSg&s=10';
+  } else if (id === 3451189) {
+    img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1WjXPIinmu1NFyT51dw-z8LdSulx91bKr9YD-V2mtaw&s=10';
+  } else if (id === 5128581) {
+    img.src = 'https://www.kkday.com/ja/blog/wp-content/uploads/america-usa-new-york-must-do.jpg';
+  } else if (id === 5368361) {
+    img.src = 'https://www.veltra.com/jp/guide/wp-content/uploads/2023/10/pixta_86309750_M.jpg';
+  }
   div.insertAdjacentElement("beforeend", img);
 }
 
 
 let button = document.querySelector('button#kekka');
-button.addEventListener('click', function () {
-  printDom(data);
-});
+button.addEventListener('click', sendRequest);
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
-
 
 
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
+  let i = document.querySelector('input[name="shimei"]');
+  let name = i.value;  
+  if(name === 'カイロ'){
+    id = 360630;
+  }
+  if(name === 'モスクワ'){
+    id = 524901 ;
+  }
+  if(name === 'ヨハネスブルグ'){
+    id = 993800;
+  }
+  if(name === '北京'){
+    id = 1816670;
+  }
+  if(name === '東京'){
+    id = 1850147;
+  }
+  if(name === 'シンガポール'){
+    id = 1880252;
+  }
+  if(name === 'シドニー'){
+    id = 2147714;
+  }
+  if(name === 'ロンドン'){
+    id = 2643743;
+  }
+  if(name === 'パリ'){
+    id = 2968815;
+  }
+  if(name === 'リオデジャネイロ'){
+    id = 3451189;
+  }
+  if(name === 'ニューヨーク'){
+    id = 5128581;
+  }
+  if(name === 'ロサンゼルス'){
+    id = 5368361;
+  }
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+ id +'.json';
 
+  axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);      // 通信の最後の処理
 }
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
+  // サーバから送られてきたデータを出力
+    let data = resp.data;
 
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+        }
+
+    // data をコンソールに出力
+    console.log(data);
+    // data.x を出力
+    console.log(data.x);
+
+    printDom(data);
 }
 
 // 課題6-1: 通信エラーが発生した時の処理
@@ -107,55 +177,4 @@ function showError(err) {
 function finish() {
     console.log('Ajax 通信が終わりました');
 }
-
-////////////////////////////////////////
-// 以下はグルメのデータサンプル
-// 注意: 第5回までは以下を変更しないこと！
-// 注意2: 課題6-1 で以下をすべて削除すること
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
 
